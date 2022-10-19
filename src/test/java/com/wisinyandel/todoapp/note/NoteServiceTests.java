0e3a87ef29d3bb86c4d97fdf3d49ca4349bfe320
Erena <david.erena@solera.com>
2022-10-19 11:48:09 +0200

    Destroy test

--- src/test/java/com/wisinyandel/todoapp/note/NoteServiceTests.java
+++ src/test/java/com/wisinyandel/todoapp/note/NoteServiceTests.java
@@ -1,5 +1,7 @@
 package com.wisinyandel.todoapp.note;
 
+import static org.junit.Assert.assertNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,5 +17,12 @@ class NoteServiceTests {
         Note note = new Note(2, "Test",null, null, "link","mentions",1);
         Note result = _service.create(note);
         Assert.notNull(result, "Test");
+    }
+    
+    @Test
+    public void destroyNote_WhenCalled_IdNotFound(int id) {
+    	_service.destroy(id);
+    	Note result = _service.getById(id);
+    	assertNull(result);
     }
 }
