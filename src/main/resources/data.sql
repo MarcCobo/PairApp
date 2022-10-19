dd985d886b410676859c2bac654dec28f3ac9f61
Erena <david.erena@solera.com>
2022-10-19 12:33:09 +0200

    Created data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO notes (id, text, priority) VALUES (1, 'Brazil', 2);
+INSERT INTO notes (id, text, priority) VALUES (2, 'India', 3);
+INSERT INTO notes (id, text, priority) VALUES (3, 'USA', 1);
+INSERT INTO notes (id, text, priority) VALUES (4, 'Italy', 5);
